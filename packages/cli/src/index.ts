fc43d3726954889dc57b899e71b84e811ff7e266
papitodele <45469370+ometere123@users.noreply.github.com>
2026-09-12 00:39:45 +0100

    feat(a2): export expanded CLI commands

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./commands/policyCompile";
+export * from "./commands/policyTools";
 export * from "./commands/evidenceBuild";
 export * from "./commands/txTrack";
+export * from "./commands/readTools";
